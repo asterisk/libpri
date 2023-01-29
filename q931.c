1ad2b2e8a9b771193c9224bfb9bfaf1665590c36
David Brown <david.brown@24i.com>
2023-01-29 18:45:42 +0100

    Correct tabs

--- q931.c
+++ q931.c
@@ -7706,12 +7706,12 @@ int q931_receive(struct q921_link *link, q931_h *h, int len)
 	case MAINTENANCE_PROTOCOL_DISCRIMINATOR_1:
 	case MAINTENANCE_PROTOCOL_DISCRIMINATOR_2:
 		if (!ctrl->service_message_support) {
-	 		/* Real service message support has not been enabled (and is OFF in libpri by default),
-	 		 * so we have to revert to the 'traditional' KLUDGE of changing byte 4 from a 0xf (SERVICE)
-	 		 * to a 0x7 (SERVICE ACKNOWLEDGE) */
-	 		/* This is the weird maintenance stuff.  We majorly
-	 		   KLUDGE this by changing byte 4 from a 0xf (SERVICE)
-	 		   to a 0x7 (SERVICE ACKNOWLEDGE) */
+			/* Real service message support has not been enabled (and is OFF in libpri by default),
+			 * so we have to revert to the 'traditional' KLUDGE of changing byte 4 from a 0xf (SERVICE)
+			 * to a 0x7 (SERVICE ACKNOWLEDGE) */
+			/* This is the weird maintenance stuff.  We majorly
+			   KLUDGE this by changing byte 4 from a 0xf (SERVICE)
+			   to a 0x7 (SERVICE ACKNOWLEDGE) */
 	                h_union.raw[h->crlen + 2] -= 0x8;
 			q931_xmit(link, h, len, 1, 0);
 			return 0;
